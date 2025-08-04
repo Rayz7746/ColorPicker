461c898f4f2462c4fb4fd3b9fe292ae728b8d924
Ruiyang Zha <rzha@scu.edu>
2025-08-04 15:22:54 +0800

    Initialize React + Vite project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React,{ useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [bgColor, setbgColor] = useState('#ffffff')
+
+  return (
+    <>
+      <h1>color picker</h1>
+    </>
+  )
+}
+
+export default App
